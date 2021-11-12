f98a37398742d2a373b91af3d7115c68237e05bf
Alex Panov <panovalexsey@yandex.ru>
2021-11-12 14:23:27 +0500

    Add player card

--- androidApp/src/main/java/ru/kontur/kickerchamp/android/main/widget/PlayerCard.kt
+++ androidApp/src/main/java/ru/kontur/kickerchamp/android/main/widget/PlayerCard.kt
@@ -38,4 +38,111 @@ import ru.kontur.kickerchamp.Team
 import ru.kontur.kickerchamp.android.R
 import ru.kontur.kickerchamp.android.theme.AppTheme
 
-// TODO: Not implemented
+@Composable
+fun PlayerCard(
+    player: Player,
+    onAddOrEditPlayer: (Player) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val shape = when (player) {
+        is BlueDefender -> RoundedCornerShape(topEnd = 100.dp)
+        is BlueForward -> RoundedCornerShape(bottomEnd = 100.dp)
+        is RedDefender -> RoundedCornerShape(topStart = 100.dp)
+        is RedForward -> RoundedCornerShape(bottomStart = 100.dp)
+    }
+
+    Card(
+        modifier = Modifier.size(150.dp).then(modifier),
+        shape = shape,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            if (player is Forward) {
+                PlayerTitle("Forward", R.drawable.ic_forward)
+            }
+            Spacer(Modifier.weight(1f))
+            AddOrEditPlayerButton(player, onAddOrEditPlayer)
+            Spacer(Modifier.weight(1f))
+            if (player is Defender) {
+                PlayerTitle("Defender", R.drawable.ic_defender)
+            }
+        }
+    }
+}
+
+@Composable
+private fun PlayerTitle(
+    title: String,
+    @DrawableRes iconRes: Int
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(title, style = MaterialTheme.typography.h6)
+        Spacer(Modifier.width(8.dp))
+        Icon(painterResource(iconRes), contentDescription = null)
+    }
+}
+
+@Composable
+private fun AddOrEditPlayerButton(
+    player: Player,
+    onClick: (Player) -> Unit
+) {
+    val color = when (player as Team) {
+        is BlueTeam -> MaterialTheme.colors.primary
+        is RedTeam -> MaterialTheme.colors.secondary
+    }
+
+    if (player.name.isNotEmpty()) {
+        Text(
+            player.name,
+            style = MaterialTheme.typography.h5,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = color,
+            modifier = Modifier.clickable { onClick(player) }
+        )
+    } else {
+        Button(
+            onClick = { onClick(player) },
+            shape = CircleShape,
+            modifier = Modifier.size(64.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = color)
+        ) {
+            Icon(Icons.Default.Add, contentDescription = null)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PlayerCardBlueForwardPreview() {
+    AppTheme {
+        PlayerCard(BlueForward(), {})
+    }
+}
+
+@Preview
+@Composable
+fun PlayerCardBlueDefenderPreview() {
+    AppTheme {
+        PlayerCard(BlueDefender(), {})
+    }
+}
+
+@Preview
+@Composable
+fun PlayerCardRedForwardPreview() {
+    AppTheme {
+        PlayerCard(RedForward(), {})
+    }
+}
+
+@Preview
+@Composable
+fun PlayerCardRedDefenderPreview() {
+    AppTheme {
+        PlayerCard(RedDefender(), {})
+    }
+}
